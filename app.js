32400b67f416b0f665d4c01d1dbd97a66c7dd76d
Adam Larson <larson.adam12@gmail.com>
2018-05-07 13:01:32 -0500

    party code logic

--- app.js
+++ app.js
@@ -11,10 +11,19 @@ var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+//const URL_ADDRESS = "192.168.1.3";
+const URL_ADDRESS = "localhost";
 
 var client_id = 'a00ebad9444f4848b35b79bb9f225cbd'; // Your client id
 var client_secret = 'b216eba609be4c0fb0148c678732fc98'; // Your secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var redirect_uri = 'http://' + URL_ADDRESS + ':8888/callback'; // Your redirect uri
+var token_arr = new Array();
+var party_code_arr = new Array();
+var email_list = new Array();
+
 
 /**
  * Generates a random string containing numbers and letters
@@ -35,8 +44,26 @@ var stateKey = 'spotify_auth_state';
 
 var app = express();
 
+// setup to receive json posts
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.use(express.static(__dirname + '/public'))
    .use(cookieParser());
+
+function hostLoginPromise(options) {
+  return new Promise(function(resolve, reject) {
+    request.get(options, function(error, response, body) {
+      if (error) {
+        reject(error);
+      }
+      current_email = body.email;
+      console.log(current_email);
+      resolve(body.email);
+    })
+  });
+}
 
 app.get('/login', function(req, res) {
 
@@ -85,6 +112,8 @@ app.get('/callback', function(req, res) {
     };
 
     request.post(authOptions, function(error, response, body) {
+      var party_code = generateRandomString(4);
+      var current_email = "";
       if (!error && response.statusCode === 200) {
 
         var access_token = body.access_token,
@@ -96,22 +125,32 @@ app.get('/callback', function(req, res) {
           json: true
         };
 
-        // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
-          console.log(body);
+        hostLoginPromise(options).then(function(data) {
+          console.log(data);
+          var current_index = email_list.indexOf(data);
+          if(current_index === -1) {
+            party_code_arr[party_code_arr.length] = party_code;
+            token_arr[token_arr.length] = access_token;
+            email_list[email_list.length] = data;
+          }
+          else {
+            party_code = party_code_arr[current_index];
+            access_token = token_arr[current_index];
+          }
+          res.cookie("pc", party_code);
+          res.redirect('/host.html');
+        }).catch(function(err) {
+          console.log("ERROR: ", err);
         });
 
-        // we can also pass the token to the browser to make requests from there
-        res.redirect('/#' +
-          querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-          }));
       } else {
         res.redirect('/#' +
           querystring.stringify({
             error: 'invalid_token'
           }));
+      }
+      function setCurrentEmail(theEmail) {
+        current_email = theEmail;
       }
     });
   }
@@ -135,17 +174,62 @@ app.get('/refresh_token', function(req, res) {
     if (!error && response.statusCode === 200) {
       var access_token = body.access_token;
       res.send({
-        'access_token': access_token
+        'access_token': access_token,
       });
     }
   });
 });
 
-app.get('/check-code', function(error, response, body) {
-  res.send({
-    'code_confirm': req.body
-  })
-})
+app.get('/getPlaylist', function(request, response) {
+  console.log(request.body.pc);
+  var aOptions = {
+    url: 'https://api.spotify.com/v1/users/{user_id}/playlists/{playlist_id}',
+    headers: { 'Authorization': 'Bearer ' + access_token },
+    json: true
+  }
+});
+
+app.post('/addSong', function(request, response) {
+  //console.log(request.body);
+  if (request.body.partyCode === '1234')
+    response.sendStatus(200);
+  else
+    response.sendStatus(404);
+});
+
+function emailAlreadyRegistered(theEmail) {
+  for(var i = 0; i < email_list.length; i++) {
+    if(email_list[i] === theEmail) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function findIndexByEmail(theEmail) {
+  for(var i = 0; i < email_list.length; i++) {
+    if(email_list[i] == theEmail) {
+      console.log(i + " : " + theEmail + " : " + email_list[i]);
+      return i;
+    }
+  }
+  return -1;
+}
 
 console.log('Listening on 8888');
 app.listen(8888);
+
+/*
+callFirstPromise().then(function(data) {
+  // do stuff with data
+  return callSecondPromise();
+}).then(function(data) {
+  // do stuff with new data
+  return callThirdPromise();
+}).then(data => {
+  return callFourthPromise();
+  //finish
+}).catch(function(err) {
+  // any errors from promises are caught here
+  console.log(err);
+});*/
